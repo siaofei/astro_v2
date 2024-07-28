0cddf413dfebcc6843341fc3905de91cb946d42a
Xiaofei Liu <xiaofei1356@gmail.com>
2024-07-28 18:16:06 +0800

    styles: add @stylistic/eslint-plugin and unicorn

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
 // @ts-check
 import eslint from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
 import eslintConfigPrettier from 'eslint-config-prettier'
 import eslintPluginAstro from 'eslint-plugin-astro'
+import eslintPluginUnicorn from 'eslint-plugin-unicorn'
 import tseslint from 'typescript-eslint'
 
 import globals from 'globals'
@@ -14,6 +16,9 @@ export default tseslint.config(
   ...tseslint.configs.recommended, // ts
   ...eslintPluginAstro.configs.recommended, // astro
   ...eslintPluginAstro.configs['flat/jsx-a11y-recommended'], // astro components A11Y
+  /* @ts-expect-error languageOptions emcaVersion type incompatible */
+  eslintPluginUnicorn.configs['flat/recommended'],
+  stylistic.configs['recommended-flat'],
   {
     languageOptions: {
       ecmaVersion: 'latest',
@@ -30,10 +35,24 @@ export default tseslint.config(
       'no-irregular-whitespace': 'warn',
       'prefer-const': 'warn',
       'no-unused-vars': 'warn',
+      'astro/jsx-a11y/iframe-has-title': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-non-null-assertion': 'off',
       '@typescript-eslint/no-unused-vars': 'warn',
-      'astro/jsx-a11y/iframe-has-title': 'off',
+      '@stylistic/quotes': 'off',
+      '@stylistic/spaced-comment': ['warn', 'always'],
+      '@stylistic/max-len': [
+        'warn',
+        {
+          code: 100,
+          tabWidth: 2,
+          ignoreComments: true,
+          ignoreUrls: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+        },
+      ],
+      '@stylistic/quote-props': 'off',
     },
   },
   eslintConfigPrettier, // disable formatting rules
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,7 +9,6 @@ export default {
       backgroundImage: {
         'repeating-dots':
           '-webkit-repeating-radial-gradient(center center,#777,#777 1px,transparent 0,transparent 100%)',
-        // eslint-disable-next-line quotes
         wave: "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 70 500 60' preserveAspectRatio='none'%3E%3Crect x='0' y='0' width='500' height='500' style='stroke: none; fill: %23ffffff;' /%3E%3Cpath d='M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z' style='stroke: none; fill: %23323541;'%3E%3C/path%3E%3C/svg%3E\")",
         'wave-stone':
           "url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 70 500 60' preserveAspectRatio='none'%3E%3Crect x='0' y='0' width='500' height='500' style='stroke: none; fill: %23f6f6f6;' /%3E%3Cpath d='M0,100 C150,200 350,0 500,100 L500,00 L0,0 Z' style='stroke: none; fill: %23323541;'%3E%3C/path%3E%3C/svg%3E\")",
