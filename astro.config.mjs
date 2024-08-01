92cd2dfeb952ac2ca14e23393e0fbd069ac67068
Xiaofei Liu <xiaofei1356@gmail.com>
2024-08-01 16:33:38 +0800

    fix: add site in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://siaofei.github.io',
   integrations: [
     tailwind({
       applyBaseStyles: false,
